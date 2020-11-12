e43f2df56b061b58cbbef9946fc75c591ed0ccaf
sohyunc1990 <sohyunc1990@gmail.com>
2020-11-12 12:18:20 -0600

    Create Easy 002 - Compare the Triplets.js

---
+++ Easy 001~100/Easy 002 - Compare the Triplets.js
@@ -0,0 +1,8 @@
+function compareTriplets(a, b) {
+    
+    var score = [0,0]
+    
+    for (let i = 0; i < a.length; i++)
+    a[i] > b[i] ? score[0]++ : a[i] < b[i] ? score[1]++ : ""
+    return score
+}
