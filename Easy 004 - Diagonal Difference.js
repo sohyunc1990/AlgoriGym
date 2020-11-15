454cf1cdd6365522fbcf2c136dc0abc65338964f
sohyunc1990 <66218682+sohyunc1990@users.noreply.github.com>
2020-11-15 04:02:53 -0700

    Create Easy 004 - Diagonal Difference.js

---
+++ Easy 004 - Diagonal Difference.js
@@ -0,0 +1,18 @@
+function diagonalDifference(arr) {
+    // Write your code here
+    
+    var n = arr.length;
+    var diag1 = 0;
+    var diag2 = 0;
+    for(var i=0; i<n; i++){
+        for(var j=0; j<n; j++){
+            if(i === j) { 
+                diag1 += arr[i][j];
+            }
+            if(i + j === n - 1){
+                diag2 += arr[i][j];
+            }
+        }
+    }
+    return Math.abs(diag1 - diag2);
+}
