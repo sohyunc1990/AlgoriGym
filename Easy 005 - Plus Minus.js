550626748eb870597309d09677ed19e069453bc1
sohyunc1990 <66218682+sohyunc1990@users.noreply.github.com>
2020-11-16 22:45:49 -0800

    Create Easy 005 - Plus Minus.js

---
+++ Easy 005 - Plus Minus.js
@@ -0,0 +1,17 @@
+function plusMinus(arr) {
+    var i;
+    var result = [0, 0, 0]
+    for(i=0; i < arr.length; i++){
+        if(arr[i] > 0){
+            result[0]++
+        } else if(arr[i] < 0 ){
+            result[1]++
+        } else if(arr[i] == 0) {
+            result[2]++
+        }
+    }
+    var j;
+    for(j=0; j < result.length; j++){
+            console.log(result[j]/arr.length)
+    }
+}
